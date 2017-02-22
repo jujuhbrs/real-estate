d1732e8dd677994481bd61df7258084c7e7fdc2e
julien <julien@MacBook-Pro-de-julien-4.local>
2017-02-22 13:36:25 +0100

    push

--- app.js
+++ app.js
@@ -15,9 +15,17 @@ app.use( '/assets', express.static( 'assets' ) );
 
 //makes the server respond to the '/' route and serving the 'home.ejs' template in the 'views' directory
 app.get( '/', function ( req, res ) {
-    res.render( 'home', {
-        message: 'The Home Page!'
-    });
+    var URL = req.query.urlLBC
+
+    if ( URL ) {
+        callLeBonCoin( URL, res )
+    }
+    else {
+
+        res.render( 'home', {
+            estimation: 'Estimation du prix d'un bien
+        });
+    }
 });
 
 
@@ -25,3 +33,58 @@ app.get( '/', function ( req, res ) {
 app.listen( 3000, function () {
     console.log( 'App listening on port 3000!' );
 });
+
+
+function callLeBonCoin( _url, res ) {
+    request( _url, function ( error, response, body ) {
+        if ( !error && response.statusCode == 200 ) {
+            var url = cheerio.load( body )
+            var firstPrice = url( 'span.value' ).eq( 0 ).text()
+
+            var price = url( 'span.value' ).eq( 0 ).text().replace( '€', '' )
+            price = price.replace( / /g, "" )
+
+
+
+            console.log( price )
+            var Ville = url( 'span.value' ).eq( 1 ).text().split( ' ' )[0]
+            var surf = url( 'span.value' ).eq( 4 ).text().split( ' ' )[0]
+            var codeP = url( 'span.value' ).eq( 1 ).text().split( ' ' )[1]
+            var type = url( 'span.value' ).eq( 2 ).text()
+            console.log( surf )
+            var prixm2 = price / surf
+            console.log( prixm2 )
+
+
+            request( 'http://www.meilleursagents.com/prix-immobilier/' + Ville.toLowerCase() + '-' + codeP, function ( error, response, body ) {
+                if ( !error && response.statusCode == 200 ) {
+                    var url2 = cheerio.load( body )
+                    var prixAppart = url2( 'div.small-4.medium-2.columns.prices-summary__cell--median' ).eq( 0 ).text().replace( '€', '' ).replace( /\s/g, '' )
+                    prixAppart = parseFloat( prixAppart )
+                    var prixMaison = url2( 'div.small-4.medium-2.columns.prices-summary__cell--median' ).eq( 1 ).text().replace( '€', '' ).replace( /\s/g, '' )
+                    var estimation = ' '
+                    if ( type == 'Maison' ) {
+                        if ( prixMaison < prixm2 ) {
+                            estimation = 'Mauvais deal'
+                        }
+                        else {
+                            estimation = 'bondeal deal'
+                    }
+                    else {
+                        if ( prixMaison < prixm2 ) {
+                            estimation = 'Mauvais deal'
+                        }
+                        else {
+                            estimation = 'Bon deal'
+                        }
+
+                    }
+
+                    res.render( 'home', {
+                        estimation: estimation
+                    });
+                }
+            })
+        }
+    })
+}
